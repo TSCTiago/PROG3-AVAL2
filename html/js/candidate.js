e6953151767034456cd48e5fd3c068267a85fecd
Luis Brasil <48132029+Capitaozila@users.noreply.github.com>
2023-02-05 15:41:06 -0300

    add candidate.js

---
+++ html/js/candidate.js
@@ -0,0 +1,41 @@
+const selectCandidates = document.querySelector('#candidato')
+
+
+selectCandidates.addEventListener('change', searchDataCandidate)
+
+function searchDataCandidate(event) {
+
+    const value = selectCandidates.options[selectCandidates.selectedIndex].text;
+    const xhr = new XMLHttpRequest()
+
+    xhr.open('POST', '/search_data_candidate', true)
+
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    const body = {
+        name: `${value}`
+    }
+
+    const json = JSON.stringify(body)
+    xhr.send(json)
+
+    xhr.onload = (event) => {
+        responseDataCandidate(xhr, event)
+    }
+}
+
+function responseDataCandidate(xhr, event) {
+
+    if (xhr.status != 200) {
+        return;
+    }
+
+    console.log(xhr.response)
+    const data = JSON.parse(xhr.response)
+    console.log(data)
+    const tbody = document.querySelector('.table-body')
+    const status = data.cand_status == 1 ? 'Eleito' : 'Não Eleito'
+    const votes = Intl.NumberFormat('pt-br').format(data.cand_votos)
+    tbody.innerHTML = `<tr><td>${data.cand_nome}</td><td>${data.cargo_nome}</td><td>${votes}</td><td>${status}</td></tr>`
+
+}
