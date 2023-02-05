e859ac69d551311508b6d2b6c8d9466c0aa4f882
Francisco Mendes <fmmagalhaesfilho@aluno.uespi.br>
2023-02-05 19:41:30 -0300

    fixing bugs

---
+++ html/js/components/btn-menu.js
@@ -0,0 +1,7 @@
+function candidate() { window.open(window.location.replace("pages/candidate.html")) }
+
+function cargo() { window.open(window.location.replace("pages/cargo.html")) }
+
+function municipio() { window.open(window.location.replace("pages/municipio.html")) }
+
+function resultadoGeral() { window.open(window.location.replace("pages/resultadoGeral.html")) }
