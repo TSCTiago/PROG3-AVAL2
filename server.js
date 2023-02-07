530963f90d86c5ac9f5d45239ead5f8693e2145a
Tiago Silva <87677785+TSCTiago@users.noreply.github.com>
2023-02-07 12:50:15 -0300

    finalizing the project

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const endpoints = require('./endpoints')
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('./html'))
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 
 app.post('/populate_select', endpoints.populateSelect)
 app.post('/search_data_candidate', endpoints.searchCandidate)
