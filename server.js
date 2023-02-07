5ee36d376feed056624b633f0c436c7012b8914e
Tiago Silva <87677785+TSCTiago@users.noreply.github.com>
2023-02-07 12:03:56 -0300

    remove comments

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-
-// Load express module
 const express = require('express')
-// Load endpoints module
+
 const endpoints = require('./endpoints')
 
-// Create an express instance
 const app = express()
 
-// Middleware to serve static files
 app.use(express.static('./html'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// Create endpoits
 app.post('/populate_select', endpoints.populateSelect)
 app.post('/search_data_candidate', endpoints.searchCandidate)
 app.post('/search_data_candidate_by_cargo', endpoints.searchCandidateByCargo)
@@ -20,7 +15,6 @@ app.post('/search_data_candidate_by_municipio', endpoints.searchCandidateByMunic
 app.post('/geral_result', endpoints.generalResult)
 
 
-// Listen at 8080 port
 app.listen(8080, function () {
     console.log('Server listenning localhost:8080')
 })
